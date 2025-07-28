c32021e5c29ee2519f1507291828be99de278217
Abhishek Yadav <abhishekyadavv9990@gmail.com>
2025-07-28 15:06:18 +0530

    init

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,9 +2,16 @@
 import Chai from "./chai"
 
 function App() {
+const username = "chai aur code"
 
   return (
+    <>
     <Chai />
+    <h1>chai aur reach {username} </h1>
+    <p>test para</p>
+
+
+    </>
   )
 }
 
